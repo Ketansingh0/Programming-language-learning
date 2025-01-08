3679fd22a808f248d6ea34fbd6a8505450db4432
Ketan Singh <142403737+Ketansingh0@users.noreply.github.com>
2025-01-08 16:53:30 +0530

    Add files via upload

---
+++ Javascrpt/Js-Chapter-1/operator.js
@@ -0,0 +1,24 @@
+// Arithmetic Operators
+let a = 5;
+let b = 2;
+
+console.log("a = ", a, "& b =", b);
+console.log("a + b = ", a + b);
+console.log("a - b = ", a - b);
+console.log("a * b = ", a * b);
+console.log("a / b = ", a / b);
+
+// Modulus Operator(%) - gives remainder
+console.log("a % b = ", a % b);
+
+// Exponention(**)
+// Increment(++) --> preIncrement(++a) --> postIncrement(a++)
+// Decrement(--) --> preIncrement(--a) --> postIncrement(a--)
+// Assignment Operators(+=, -=, %=, /=, *=)
+// Comparison Operators(==, ===, !=, !==, >, >=, <, <=)
+let c = 5;
+let d = "5"; // string --> number then compare with c thats why we get true.
+console.log("c == d", c == d);
+console.log("c === d", c === d); // It will strictly checks both value and datatype hence will give false for c and d.
+
+// Logical Operator(&&, ||, !)
