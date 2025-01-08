12323527d881e4cc0d643c1470f15039d8de8a38
Ketan Singh <142403737+Ketansingh0@users.noreply.github.com>
2025-01-08 16:54:03 +0530

    Add files via upload

---
+++ Javascrpt/Introduction/first.js
@@ -0,0 +1,5 @@
+var name = "Ketan Singh";      // Variable can be re-declared and updated. A global scope variable.
+var name = "Adarsh Singh";    
+let age = 22;                  // Variabel cannot be re-declared but can be updated. A block scope variable.
+age = 20;
+const a = "Hello world!!";     // Variable cannot be re-declared or updated. A block scope variable.
