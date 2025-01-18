ded999dc4d8f9bf1dc4dd266c7efc857c6c5058e
Ketan Singh <142403737+Ketansingh0@users.noreply.github.com>
2025-01-18 20:31:01 +0530

    Add files via upload

---
+++ Javascrpt/Js-Chapter-3/Arrays.js
@@ -0,0 +1,19 @@
+// Create Array
+let marks = [97,82, 75, 64, 36];
+console.log(marks);
+console.log(marks.length);
+console.log(typeof marks);
+
+
+// Looping over an Array
+// for loop: length
+for(let i = 0; i < marks.length; i++){
+    console.log(marks[i]);
+}
+
+// for of 
+let heroes = ["Ironman", "Superman", "Spiderman", "Thor", "Hulk"];
+for(let hero of heroes){
+    console.log(hero);
+}
+
