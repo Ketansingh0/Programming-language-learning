9b200adbdc03a4ffe332589fa67a90eb4fcc6787
Ketan Singh <142403737+Ketansingh0@users.noreply.github.com>
2025-01-19 23:03:01 +0530

    Add files via upload

---
+++ Javascrpt/Js-Chapter-4/arrowFunction.js
@@ -0,0 +1,22 @@
+// Compact way of writing a function
+
+// sum function 
+function sum(a, b){
+    return a + b;
+}
+// arrow function
+const arrowSum = (a, b) => {
+    return a  + b;
+};
+console.log(arrowSum(3,4));
+
+// multiplication function
+function mul(a, b) {
+    return a * b;
+}
+
+// arrow function of multiplication
+const arrowMul = (a, b) => {
+    return a * b;
+}
+console.log(arrowMul(3, 4));
