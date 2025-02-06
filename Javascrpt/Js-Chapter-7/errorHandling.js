27fcfe3a4e76e3f5bec1ff23aeb54cfaaea7aa80
Ketan Singh <142403737+Ketansingh0@users.noreply.github.com>
2025-02-06 18:54:15 +0530

    Add files via upload

---
+++ Javascrpt/Js-Chapter-7/errorHandling.js
@@ -0,0 +1,12 @@
+// Error handling in JavaScript
+// 1. try...catch
+// 2. throw
+// 3. finally
+
+try{
+    console.log('I am line 1');
+} catch(error){
+    console.log('I am catch block and error is: ', error);
+} finally{
+    console.log('I am finally block');
+}
